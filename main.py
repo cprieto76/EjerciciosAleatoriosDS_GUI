169282cb537ef777a4aa62b7502d21466699a7b3
cprieto76 <115907710+cprieto76@users.noreply.github.com>
2023-01-27 21:29:58 -0500

    Update main.py
    Adcion de comentarios para estudio de clases y metodos de Python y del modulo Tkinter desde la linea 15 a la 49

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ lista_preguntas = []
 raiz = Tk()  # Esta funcion del modulo tkinter crea una aplicacion tkinter. Crea una ventana nivel alto (top-level). La funcion retorna 
              #, un objeto asignado tipicamente a una variable
 
-  raiz.title("preguntas")
+raiz.title("preguntas") # title() es un metodo de la clase Tk que coloca titulo de la ventana
 
 def seleccion_preguntas():
     for numero in diccionario_preguntas:
         lista_preguntas.append(numero)
-    random_index = random.randint(0, len(lista_preguntas) - 1)
+    random_index = random.randint(0, len(lista_preguntas) - 1)   # randint() es una función incorporada del modulo random que retorna 
+                                                                 # entero aleatorio entre dos numeros dados (inclusive).
     print("dentro de seleccion de preguntas")
     print(lista_preguntas[random_index])
 
@@ -28,16 +29,21 @@ def preguntas():
 
 
 def click_boton_pregunta():
-    texto = Label(raiz, text= preguntas()).grid()
-
+    texto = Label(raiz, text= preguntas()).grid()  #Label es una clase del modulo Tkinter, se utiliza para crear un widget (artilugo,
+                                                   # interfase) para mostrar texto o imagenes.
+                                                   # El metodo grid() llama entonces el objeto label, que empaqueta la etiqueta en la 
+                                                   # ventana principal utilizando el administrador de geometría de cuadrícula.
 def click_boton_hita():
     texto = Label(raiz, text= preguntas()).grid()
 
 
 boton_pregunta = Button(raiz, text="pregunta", padx=50, pady=25, command=click_boton_pregunta).grid(row=1, column=1)
+# La clase Button se utiliza para crear un widget de botón en el que el usuario puede hacer clic para realizar una acción.
 
 boton_hit = Button(raiz, text="hit!", padx=50, pady=25, command=click_boton_hita).grid(row=1, column=4)
 
 preguntas()
 
 raiz.mainloop()
+# mainloop() es un método de la clase Tkinter Tk, que es la clase principal para crear una aplicación Tkinter. Este método ingresa 
+# a un ciclo infinito que espera que ocurran los eventos y los procesa a medida que ocurren.
